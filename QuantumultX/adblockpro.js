c3312aeba3ccc9f9971b54985bb0b0814e40c3ab
TracyMock21 <tracytracymock@gmail.com>
2025-03-24 12:13:17 +0800

    Create adblockpro.js

---
+++ QuantumultX/adblockpro.js
@@ -0,0 +1,39 @@
+/*
+📜 ✨ AdblockPRO ✨
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+#!desc=浏览器广告拦截
+#!category=🔐APP
+#!author=🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+#!icon=https://raw.githubusercontent.com/Mikephie/icons/main/icon/adblockpro.png
+
+[rewrite_local]
+^https:\/\/api\.adblockpro\.app\/verify url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/adblockpro.js
+
+[MITM]
+hostname = api.adblockpro.app
+
+*/
+
+// 主脚本函数...
+let body = $response.body;
+if (!body) { $done({}); }
+
+body = JSON.parse(body);
+['p', 's', 'l'].forEach(key => {
+    if (body.hasOwnProperty(key)) body[key] = 1;
+});
+if (body.hasOwnProperty('v')) body.v = true;
+// 主脚本函数...
+
+/********** 应用配置信息 **********/
+const cooldownMs = 10 * 60 * 1000;
+const notifyKey = "AdblockPRO_notify_key";
+const now = Date.now();
+let lastNotifyTime = $persistentStore.read(notifyKey) ? parseInt($persistentStore.read(notifyKey)) : 0;
+
+if (now - lastNotifyTime > cooldownMs) {
+    $notification.post("✨AdblockPRO✨", "🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ", "永久解锁或 ⓿❽-⓿❽-❷⓿❽❽");
+    $persistentStore.write(now.toString(), notifyKey);
+}
+
+$done({ body: JSON.stringify(body) });
